b17409b9120bac84401a6b1543c39ad076d04c54
OmidMerikhi <omidi.merikhi1999@gmail.com>
2023-02-24 10:15:54 +0330

    --

--- src/main/java/com/zitel/Tamrin/TamrinApplication.java
+++ src/main/java/com/zitel/Tamrin/TamrinApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TamrinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TamrinApplication.class, args);
+
 	}
 
 }
