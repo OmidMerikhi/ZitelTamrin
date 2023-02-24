bb97f2bed05535dcf00c93719e8001b6cef326b8
OmidMerikhi <omidi.merikhi1999@gmail.com>
2023-02-24 10:22:46 +0330

    added database folder and comment in main class

--- src/main/java/com/zitel/Tamrin/TamrinApplication.java
+++ src/main/java/com/zitel/Tamrin/TamrinApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TamrinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TamrinApplication.class, args);
-
+		//main class
 	}
 
 }
